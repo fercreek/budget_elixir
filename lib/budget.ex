4ca01d9ea25ba0d6cdc494778a2379c0516fc22c
Fernando Contreras <fercreek@gmail.com>
2017-10-15 21:51:06 -0500

    Parsing and filtering data

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,5 +1,16 @@
 defmodule Budget do
+	alias NimbleCSV.RFC4180, as: CSV
 	def list_transactions do
 		File.read!("lib/transactions.csv")
+		|> parse
+		|> filter
+	end
+
+	defp parse(string) do
+		CSV.parse_string(string)
+	end
+
+	defp filter(rows) do
+		Enum.map(rows, fn(row) -> Enum.drop(row, 1) end)
 	end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Budget.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :nimble_csv]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule Budget.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:nimble_csv, "~> 0.2.0"}]
   end
 end
